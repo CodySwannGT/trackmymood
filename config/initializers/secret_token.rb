b3bbeec9970520a27def2c19329cb252a3364015
Cody Swann <cody@gunnertech.com>
2013-03-17 11:59:07 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trackmymood::Application.config.secret_token = '04e0b3954665d18012799964901a7fd4578f4dcf5c3676abb236de1ca26089c70153330787aa657c76c7411ff3bc025aeee158981943eb28cb249a80d5153f94'
